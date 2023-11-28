d573993775e82b055dad50f8688d1b0338b68644
fknipp <franz@qnipp.com>
2023-11-28 22:54:21 +0100

    Add animationFrame promise

---
+++ demo/animationframe.js
@@ -0,0 +1,17 @@
+import { clickEvent } from "../lib/events.js";
+import { animationFrame } from "../lib/timeout.js";
+import { consoleLog } from "./console.js";
+
+let previousTimestamp = 0;
+
+await clickEvent("button");
+
+while (true) {
+  const timestamp = await animationFrame();
+  const delta = timestamp - previousTimestamp;
+  previousTimestamp = timestamp;
+
+  consoleLog(
+    `Got animation frame after ${delta} milliseconds (${1000 / delta} fps).`
+  );
+}
